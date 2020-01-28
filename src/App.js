0a02b0daf786e308c1d73a93a1b0cb4aedd2b465
Daan Ruijter <daanruijter@gmail.com>
2020-01-28 11:38:01 +0100

    created landing page for the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import './App.css';
+// import TestComponent from './TestComponent'
+import MYtineraryLogo from './Pictures/MYtineraryLogo.png';
+import startBrowsing from './Pictures/startBrowsing.png';
+import homeIcon from './Pictures/homeIcon.png';
+
+class App extends Component {
+
+render(){
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <img className = "MYtineraryLogo" src = {MYtineraryLogo} alt="MYtineraryLogo"/>
+      <p>
+        Find your perfect trip, designed by insiders who know and love their cities. 
+      </p>
+
+      <h1 className="start-browsing-header">Start browsing</h1>
+      <div className = "start-browsing-flexer">
+    <img className = "startBrowsing" src = {startBrowsing} alt ="startBrowsing"/>
+    </div>
+    <p>
+        Want to build your own MYtinerary?
+      </p>
+     
+     {/* those will be UI/functional components */}
+     <div className = "UI-flexer">
+     <div className = "login-box">
+    <a href = "">Log in</a>
+     </div>
+     <div className = "login-box">
+     <a href = "">Create Account</a>
+     </div>
+     </div>
+     <div className = "home-flexer">
+     <img className = "homeIcon" src = {homeIcon} alt ="homeIcon"/>
+      </div>
     </div>
   );
 }
+}
 
 export default App;
+
+
+/* <TestComponent greet="hello" weather= "it rains" /> */
