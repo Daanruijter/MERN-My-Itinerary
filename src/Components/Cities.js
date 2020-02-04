3ffda7701a96ba95de8366d490b5decb1505239f
Daan Ruijter <daanruijter@gmail.com>
2020-02-04 11:09:13 +0100

    Cities are displayed on the screen

--- src/Components/Cities.js
+++ src/Components/Cities.js
@@ -8,41 +8,16 @@ const citiesToDisplay = ""
 
 
 
-
-// const numbers = [1, 2, 3, 4, 5];
-// const listItems = numbers.map((number) =>
-//   <li>{number}</li>
-// );
-
-// let cities = [
-//     {
-//         "_id": "5e3827091c9d4400006098dc",
-//         "name": "Amsterdam",
-//         "country": "Netherlands"
-//     },
-//     {
-//         "_id": "5e3827931c9d4400006098dd",
-//         "name": "Barcelona",
-//         "country": "Spain"
-//     }
-// ]
-
-
-
 export default class Cities extends Component {
 
 
   state = {
-    isFetching: false
+    isFetching: true
   }
 
  
   
-    // getInitialState = function (){
-    //     return ({
-    //         cities: []
-    //     })
-    // }
+
 
     componentDidMount(){
    this.setState({...this.state, isFetching: true})
@@ -71,40 +46,27 @@ export default class Cities extends Component {
 
     
     render(props) {
+
+      let listItemsMap =''
      
+     if(this.state.isFetching !== true ){
+     
+
+
+         const {cities} = this.state;
+         listItemsMap = cities.map((cityMapper) =>
+
+         <li key={cityMapper._id}>{cityMapper.name}</li> 
+         );
+     }
      
       
-      // const {cities} = this.state;
-      //   const listItemsMap = cities.map((city) =>
-
-      //   <li key={city.id}>{city.name}</li>
-      //   );
-        
-
-
-      //   console.log(cities)
-
       
-
-      
-            
-         
-                // console.log(this.state.quotes)
-        //    let  cities = this.state.quotes;
-            // console.log(cities)
-
-            // cities = cities.map(function(city, index){
-            // return (<li key={index}>
-            //     {city}
-            // </li>)
-            // })
-     
         return (
             
             <div>
-            
-              {/* {listItems} */}
-              {/* {listItemsMap} */}
+           
+              {listItemsMap}
           
             </div>
         )
