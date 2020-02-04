e2559dea7edcc4751d63557bfa589391c0e89426
Daan Ruijter <daanruijter@gmail.com>
2020-02-04 10:05:46 +0100

    needing to solve asynchronity issues

--- src/Components/Cities.js
+++ src/Components/Cities.js
@@ -5,31 +5,38 @@ const cityURL = "http://localhost:5000/cities/all"
 const citiesToDisplay = ""
 
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) =>
-  <li>{number}</li>
-);
 
-let cities = [
-    {
-        "_id": "5e3827091c9d4400006098dc",
-        "name": "Amsterdam",
-        "country": "Netherlands"
-    },
-    {
-        "_id": "5e3827931c9d4400006098dd",
-        "name": "Barcelona",
-        "country": "Spain"
-    }
-]
 
-const listItemsMap = cities.map((city) =>
 
-<li>{city.name}</li>
-);
+
+// const numbers = [1, 2, 3, 4, 5];
+// const listItems = numbers.map((number) =>
+//   <li>{number}</li>
+// );
+
+// let cities = [
+//     {
+//         "_id": "5e3827091c9d4400006098dc",
+//         "name": "Amsterdam",
+//         "country": "Netherlands"
+//     },
+//     {
+//         "_id": "5e3827931c9d4400006098dd",
+//         "name": "Barcelona",
+//         "country": "Spain"
+//     }
+// ]
+
 
 
 export default class Cities extends Component {
+
+
+  state = {
+    isFetching: false
+  }
+
+ 
   
     // getInitialState = function (){
     //     return ({
@@ -49,18 +56,36 @@ export default class Cities extends Component {
             isFetching: false})
           console.log(result);
           console.log(this.state)
-
+              
+       
                   
-        });
-
-    }
+        }
+        
+        
+        );
 
         
-     
+    }
+ 
+ 
 
     
     render(props) {
-        // console.log(this.state.quotes)  
+     
+     
+      
+      // const {cities} = this.state;
+      //   const listItemsMap = cities.map((city) =>
+
+      //   <li key={city.id}>{city.name}</li>
+      //   );
+        
+
+
+      //   console.log(cities)
+
+      
+
       
             
          
@@ -77,8 +102,9 @@ export default class Cities extends Component {
         return (
             
             <div>
-              {listItems}
-              {listItemsMap}
+            
+              {/* {listItems} */}
+              {/* {listItemsMap} */}
           
             </div>
         )
