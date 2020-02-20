99742047762db412e1bda2ed3778807dd69a32b7
Daan Ruijter <daanruijter@gmail.com>
2020-02-20 15:34:44 +0100

    click logic working for itinerary activity elements

---
+++ src/Components/Activities.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import {Itinerary} from "./Itinerary";
+import '../CSS/Activities.css';
+
+export default class Activities extends Component {
+
+    openCaroussel = function () {
+    
+        this.setState(prevState => ({ 
+            
+            carousselOpen: !prevState.carousselOpen,
+         
+      
+             }))
+    
+     }
+    
+         handleClick = () => {
+             console.log("HANDLECLICK")
+    
+             this.openCaroussel()
+           
+    
+    
+    
+        }
+          
+
+    state ={
+        carousselOpen:false,
+   
+
+        
+     
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="itinenary-click-here-fore-more-information">
+            Click <a href = {this.props.itinerary.moreInformation}>here</a> for more information
+            </div>
+            <div className ="also" key={this.props.index} matcher = {this.props.itinerary._id} onClick = {()=> {this.handleClick()}} 
+            
+            className="activities-also-nice">
+            ALSO NICE TO DO</div>
+            
+            {this.state.carousselOpen?
+                    <div className="activities-content" > 
+         show me<br/>
+         show me<br/>
+         show me<br/>
+         show me<br/>
+         show me<br/>
+         show me<br/>
+         show me<br/>
+         show me<br/>
+         <p className="activities-close" key={this.props.index} matcher = {this.props.itinerary._id} onClick = {()=> {this.handleClick()}}>close</p>
+         
+        </div>
+        : null  }
+            </div>
+        )
+    }
+}
--- src/Components/Itinerary.js
+++ src/Components/Itinerary.js
@@ -1,5 +1,5 @@
 
-
+import Activities from "./Activities";
 import {connect} from 'react-redux'
 import {fetchItineraries} from '../store/actions/itineraryActions'
 
@@ -15,64 +15,13 @@ import homeIcon from '../Pictures/homeIcon.png';
 
 class Itinerary extends Component {
    
-state ={
-        carousselOpen:false,
-        carousselOpenNum: '',
-         [this.props.collectedId]:false
-     
-    }
 
    
-  openCaroussel = function (test, itinerariesForSpecificCity, collectedId) {
-    console.log(this.state)
-    // console.log(test)
-    // console.log(collectedId)
-    // console.log(itinerariesForSpecificCity[test].key)
-    this.setState(prevState => ({ 
-        carousselOpen: !prevState.carousselOpen,
-     
-  
-         }))
-
- }
-
-     handleClick = (test, itinerariesForSpecificCity, collectedId) => {
-         console.log("HANDLECLICK")
-        console.log(this.state)
-        this.setState( ({
-            [collectedId]:test
-           
-        }))
-
-
-        console.log(collectedId)
-        // this.setState(collectedId => ({ 
-        //     collectedId: 2}))
-        //  var test = e
-       console.log(test) 
-
-    //    if(itinerariesForSpecificCity[test].key===collectedId && itinerariesForSpecificCity[test].key!==undefined && collectedId!==undefined && test===0){console.log("e")
-    //   this.openCaroussel()}
-        
-        if(test===0){console.log("e")
-      this.openCaroussel()}
-      if(test===1){console.log("e")
-      this.openCaroussel()}
-
-
-    }
-      
-       
-    
-   
-    // closeCaroussel = () => {
-    //     this.setState({showMe:false})
-    // }
 
     
     componentDidMount(){
         
-        console.log(this.index)
+        
         let cityItinerariesToBeFetched = this.props.match.params.name
      
    
@@ -84,19 +33,9 @@ state ={
  
 
 
-
-
     render() {
         
-        
-        
-     
-        
-  
-    
-
-    
-       
+               
 
                 
         let itinerariesForSpecificCity  = this.props.itineraries.map((itinerary, index) =>
@@ -129,19 +68,8 @@ state ={
             <img className ="itinerary-image-height" src ={itinerary.image}/>
             </div>
             <div className="itinenary-more-information">
-            Click <a href = {itinerary.moreInformation}>here</a> for more information
+            <Activities index={index} itinerary={itinerary}></Activities>
             </div>
-            <div key={index} matcher = {itinerary._id} onClick = {()=> {this.handleClick(index, itinerariesForSpecificCity, itinerary._id)}} className="itinerary-activities">
-            ALSO NICE TO DO</div>
-            
-            {this.state.carousselOpen?
-                    <div > 
-         show me<br/>
-         show me<br/>
-         <p key={index} matcher = {itinerary._id} onClick = {()=> {this.handleClick(index, itinerariesForSpecificCity,itinerary._id)}}>close</p>
-         
-        </div>
-        : null  }
         </div>
             
             
