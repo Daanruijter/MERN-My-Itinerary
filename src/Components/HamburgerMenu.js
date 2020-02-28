a2053afcc2b9baaea89a546987322339f8db2dea
Daan Ruijter <daan_ruijter@hotmail.com>
2020-02-28 10:07:55 +0100

    changed navbar to have a login and logout clickable button

--- src/Components/HamburgerMenu.js
+++ src/Components/HamburgerMenu.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import '../CSS/App.css';
-import '../CSS/HamburgerMenu.css'
-import DrawerToggleButton from './DrawerToggleButton'
-
-
+import React from "react";
+import "../CSS/App.css";
+import "../CSS/HamburgerMenu.css";
+import DrawerToggleButton from "./DrawerToggleButton";
 
 const HamburgerMenu = props => {
-    
-    return (
-        
+  return (
     <header className="hamburger-menu-header">
-        <nav className="hamburger-menu-navigation">
-        <div className="hamburger-flexer">
-        <DrawerToggleButton click = {props.drawerToggleClickHandler}></DrawerToggleButton>
-        </div>
-        <div className = "hamburger-menu-container" >
-        <div className = "hamburger-menu-user-logo" >
-          
-           
-            </div>  
+      <nav className="hamburger-menu-navigation">
+        <div className="hamburger-grid">
+          <div className="hamburger-login">login</div>
+          <div className="hamburger-logout">logout</div>
+
+          <div className="hamburger-icon">
+            <div className="hamburger-icon-flexer">
+              <DrawerToggleButton
+                click={props.drawerToggleClickHandler}
+              ></DrawerToggleButton>
             </div>
-        </nav>
+          </div>
+        </div>
+        <div className="hamburger-menu-container">
+          <div className="hamburger-menu-user-logo"></div>
+        </div>
+      </nav>
     </header>
-    
-)
-}
+  );
+};
 
-export default HamburgerMenu
-
-
-
+export default HamburgerMenu;
